13aeb01be218fb473dda8d703bad0670332ea631
zhangyian <canlucingning@gmail.com>
2022-12-10 21:30:39 +0800

    登录接口

---
+++ guigu-auth-parent/service-system/src/main/java/com/atguigu/system/controller/IndexController.java
@@ -0,0 +1,59 @@
+package com.atguigu.system.controller;
+
+import com.atguigu.common.result.Result;
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author ZhangYiAn
+ * @version 1.0.0
+ * @ClassName IndexController.java
+ * @Description 服务器端增加接口
+ * @createTime 2022年12月10日 21:10:00
+ */
+
+@Api(tags = "用户登录的接口")
+@RestController
+@RequestMapping("/admin/system/index")
+public class IndexController {
+
+    /**
+     * 登录接口
+     * @return
+     */
+    @PostMapping("login")
+    public Result login(){
+        Map<String,Object> map=new HashMap<>();
+        map.put("token","admin-token");
+        return  Result.ok(map);
+    }
+
+    /**
+     * info接口
+     * @return
+     */
+    @GetMapping("info")
+    public Result info(){
+        Map<String,Object> map=new HashMap<>();
+        map.put("roles","[admin]");
+        map.put("name","Super Admin");
+        map.put("avatar","https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif");//头像
+        return Result.ok(map);
+    }
+
+    /**
+     * 退出
+     * @return
+     */
+    @PostMapping("/logout")
+    public Result logout(){
+        return Result.ok();
+    }
+
+}
